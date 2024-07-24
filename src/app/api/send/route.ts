76270487ce4746af02493767cf316ae2a5899b6c
dperezch <davarmando@gmail.com>
2024-07-24 12:38:55 -0400

    api/send/route --> se trae el código de utils/sendEmail

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,8 +1,40 @@
-import { sendMail } from "@/utils/sendEmail";
 import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export async function POST(request: Request) {
   const formBody = await request.json();
+
+  const transporter = nodemailer.createTransport({
+    service: process.env.MAILER_SERVICE,
+    auth: {
+      user: process.env.MAILER_USER,
+      pass: process.env.MAILER_PASS,
+    },
+  });
+
+  interface EmailBody {
+    from: string;
+    to: string;
+    subject: string;
+    text: string;
+  }
+
+  const sendMail = async (emailBody: EmailBody) => {
+    const mailOptions = {
+      from: emailBody.from,
+      to: emailBody.to,
+      subject: emailBody.subject,
+      text: emailBody.text,
+    };
+    transporter
+      .sendMail(mailOptions)
+      .then((message) => {
+        console.log(message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   try {
     await sendMail(formBody);
